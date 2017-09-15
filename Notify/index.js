55cd1ffda47b023faa3a50f1edf1a41820a49cfd
Jan Fellien <jan.fellien@devcrowd.de>
2017-09-15 21:48:09 +0200

    add journey notification

--- Notify/index.js
+++ Notify/index.js
@@ -2,9 +2,10 @@ const request = require('request-promise');
 
 module.exports = (context, event) => {
 
-  let message = 'Hello Azure Meetup';
+  let message = event.eventName;
 
   if(event.eventName === 'car rented') { message = `Car with id: ${ event.carId } was rented` }
+  else if(event.eventName === 'journey prepared') { message = `Journey ${ event.journeyId} prepared for CarId ${ event.carId }`}
 
   const slackOptions = {
     method: 'POST',
